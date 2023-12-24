668bdd76d3346a00d6bc7e9b229b77ffdf22bf8e
shakil.ahmed <shakil.ahmed@amiprobashi.com>
2023-12-24 14:43:36 +0600

    Add Some pages objects add conftest

--- utilities/conftest.py
+++ utilities/conftest.py
@@ -1,21 +1,3 @@
 import pytest
 from selenium import webdriver
-from basic_automation.page_objects.login_page import Practice, NavigateMenu
-
-
-@pytest.fixture()
-def setup_browser():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-
-
-@pytest.fixture()
-def practice_page(setup_browser):
-    # Fixture to provide an instance of LogIn page for tests
-    return Practice(setup_browser)
-
-
-@pytest.fixture()
-def navigate_menu_page(setup_browser):
-    return NavigateMenu(setup_browser)
+from basic_auto_mation.page_objects.login_page import Practice, NavigateMenu
